f4182181bc3234c8e9d1e3e0da88555abd27167d
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-10-31 13:19:09 -0300

    Added Gameplay

--- Arrays_Ammo_AdrianDorey/Program.cs
+++ Arrays_Ammo_AdrianDorey/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Arrays_Ammo_AdrianDorey
     internal class Program
     {
         // declarations
+        static string[] Weapon; //name of weapon
         static int[] ammo;
         static int currentWeapon;
 
@@ -24,42 +26,83 @@ namespace Arrays_Ammo_AdrianDorey
             ammo[2] = 100; // spreader
             ammo[3] = 6; // rocket launcher
 
-            currentWeapon = 0; // pistol
+            Weapon = new string[4];
+            Weapon[0] = "Pistol";
+            Weapon[1] = "Shotgun";
+            Weapon[2] = "Spreader";
+            Weapon[3] = "Rocket Launcher";
 
-            
+            currentWeapon = 1; 
 
+            ShowHUD();
+            Fire();
+            Fire();
+            Fire();
+            Fire();
+            Fire();
+            ShowHUD();
+            Reload(3);
+            Fire();
+            ShowHUD();
+            Fire();
+            ShowHUD();
+            Reload(2);
+            ShowHUD();
+            Fire();
+            ShowHUD();
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey(true);
         }
 
-        // 
-        void Fire()
+        static void Fire()
         {
-            if (ammo[currentWeapon] <= 0) return;
-            ammo[currentWeapon] -= 1;
-
+            if (ammo[currentWeapon] > 0)
+            {
+                Console.WriteLine("Bang! Weapon fired!");
+                Console.WriteLine();
+                ammo[currentWeapon] -= 1;
+            }
+            else
+            {
+                Console.WriteLine("Weapon out of ammo, Reload!");
+                Console.WriteLine();
+            }
         }
 
-        void Reload(int ammoAmount)
+        static void ShowHUD()
+        {
+            Console.WriteLine("Current Weapon: " + Weapon[currentWeapon]);
+            Console.WriteLine("Amount of Ammo: " + ammo[currentWeapon]);
+            Console.WriteLine("");
+        }
+
+        static void Reload(int ammoAmount)
         {
             ammo[currentWeapon] = ammo[currentWeapon] + ammoAmount;
 
+            Console.WriteLine(Weapon[currentWeapon] + " got an extra " + ammoAmount + " rounds");
+            Console.WriteLine();
+
             if (ammo[0] >= 50) // if pistol ammo is more than 50
             {
+                Console.WriteLine("Pistol ammo is full");
                 ammo[0] = 50;
             }
             else if (ammo[1] >= 20) // if shotgun is more than 20
             {
+                Console.WriteLine("Shotgun ammo is full");
                 ammo[1] = 20;
             }
             else if (ammo[2] >= 100)    // if spreader is more than 100
             {
+                Console.WriteLine("Spreader ammo is full");
                 ammo[2] = 100;
             }
             else if (ammo[3] >= 6)  // if rocket launcher is more than 6
             {
+                Console.WriteLine("Rocket Launcher ammo is full");
                 ammo[3] = 6;
             }
         }
